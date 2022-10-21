2562e8de1f01ae45f5b24d6e2369418eeb00799e
KayvonPaygon <112442143+KayvonPaygon@users.noreply.github.com>
2022-10-20 20:08:02 -0500

    Update and rename code.py to code.md

--- code.py
+++
@@ -1,8 +0,0 @@
-import random
-
-facts = ["I'm super cool", "I was a chemistry major", "I want to be a software engineer",
-"I speak 3 languages (the third one not well)", "I'm from Puerto Rico", "I study to britney spears",
-"I dream of being a crazy cat lady", "I plan on moving to Seattle", "Crocs are approved by me",
-"I worked on a huge essay that was worth half my grade for 4 weeks and it didn't even get graded"]
-
-print(random.choice(facts))
